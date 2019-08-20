b68407012d78414b5a058c060172982686fe0507
chekjun <limchekjun@gmail.com>
2019-08-20 16:12:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String logo =
+                  " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString;
+        boolean shutdown = false;
+        while (!shutdown) {
+            inputString = scanner.nextLine();
+            if (inputString.equals("bye")) {
+                shutdown = true;
+                System.out.println("Bye. Hope to see you again soon!");
+                
+            } else {
+                System.out.println(inputString);
+            }
+        }
     }
+
 }
