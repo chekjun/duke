cf7f92410770e2b5362b9ad3f4007850bb06f19c
chekjun <limchekjun@gmail.com>
2019-08-21 20:29:23 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,21 +12,33 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("✓");
+
         String inputString;
-        ArrayList<String> storeStrings = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<Task>();
         boolean shutdown = false;
         while (!shutdown) {
             inputString = scanner.nextLine();
             if (inputString.equals("bye")) {
-                shutdown = true;
                 System.out.println("Bye. Hope to see you again soon!");
-                
+                shutdown = true;
             } else if (inputString.equals("list")) {
-                for (int i = 1; i <= storeStrings.size(); ++i) {
-                    System.out.println(i + ". " + inputString);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i <= taskList.size(); ++i) {
+                    System.out.println(i + ". ["
+                        + taskList.get(i - 1).getStatusIcon()
+                        + "] "
+                        + taskList.get(i - 1).description);
                 }
+            } else if (inputString.length() > 4 && inputString.substring(0, 4).equals("done")) {
+                System.out.println("Nice! I've marked this task as done:");
+                int doneNum = Integer.parseInt(inputString.substring(5));
+                Task t = taskList.get(doneNum - 1);
+                t.isDone = true;
+                System.out.println("[\u2713] " + t.description);
             } else {
-                storeStrings.add(inputString);
+                Task t = new Task(inputString);
+                taskList.add(t);
                 System.out.println("added: " + inputString);
             }
         }
